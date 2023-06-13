9f3a5e991873140757f35879906d072118f2bfc9
amac81 <arnaldocanelas@gmail.com>
2023-06-13 17:10:09 +0100

    folder updated

--- OOP_StaticMembers/src/util/Calculator.java
+++ OOP_StaticMembers/src/util/Calculator.java
@@ -1,9 +1,9 @@
 package util;
 
 public class Calculator {
-	public static final double PI = 3.14159;
+	public static final double PI = 3.14159;//constant static attribute
 	
-	public static double circumference(double radius) {
+	public static double circumference(double radius) { //static 
 		return 2.0 * PI * radius;
 	}
 	
