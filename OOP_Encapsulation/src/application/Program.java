92cfef96a2952008af8f56c3c15bba1b5f3b4b9d
amac81 <arnaldocanelas@gmail.com>
2023-06-14 12:54:23 +0100

    getters and setters use

--- OOP_Encapsulation/src/application/Program.java
+++ OOP_Encapsulation/src/application/Program.java
@@ -9,8 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 
-		Scanner sc = new Scanner (System.in);
-		
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Enter Person data:\n");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
@@ -21,16 +21,32 @@ public class Program {
 		System.out.print("Age: ");
 		int age = sc.nextInt();
 		System.out.print("Email: ");
-		sc.nextLine(); //clean buffer
+		sc.nextLine(); // clean buffer
 		String email = sc.nextLine();
-		
-		Person person1 = new Person(name, height, weight, age,email);
-		 
 
-		System.out.print("\nPerson Data:" + person1.toString()); // toString() can be omitted
+		Person person1 = new Person(name, height, weight, age, email);
 
+		System.out.print("\nPerson Data:" + person1.toString()); 
+
+		System.out.println("\n\n######## SHOW DATA OF PERSON #########:\n");
+
+		System.out.print("Name: " + person1.getName());
+		System.out.print("\nAge: " + person1.getAge());
+
+		System.out.println("\n######## CHANGE DATA OF PERSON #########:\n\n");
+
+		System.out.println("Enter Person data to UPDATE:\n");
+		System.out.print("Name: ");
+		name = sc.nextLine();
+		person1.setName(name);
+
+		System.out.print("Email: ");
+		email = sc.nextLine();
+		person1.setEmail(email);
 		
-		sc.close();		
+		System.out.print("\nPerson Data:" + person1.toString());
+
+		sc.close();
 	}
 
 }
