e0c0c54296ed2bc17206c79bf0be655f4c17cca2
amac81 <arnaldocanelas@gmail.com>
2023-06-14 11:21:52 +0100

    added class CurrencyConverter

--- OOP_StaticMembers/src/application/Program.java
+++ OOP_StaticMembers/src/application/Program.java
@@ -4,12 +4,15 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import util.Calculator;
+import util.CurrencyConverter;
 
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 	
+		System.out.println("CALCULATOR\n");
+		
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 		double c = Calculator.circumference(radius);
@@ -18,6 +21,22 @@ public class Program {
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
 		System.out.printf("PI value: %.2f%n", Calculator.PI);
+		
+		
+		System.out.println("CURRENCY CONVERTER\n");
+		System.out.print("What is the dollar price? ");
+		double dollarPrice = sc.nextDouble();
+		
+		System.out.print("How many dollars will be bought? ");
+		int quantityToBuy = sc.nextInt();
+		
+		double amountToPay = CurrencyConverter.dollarsToEuros(dollarPrice, quantityToBuy);
+		
+		System.out.printf("Amount to be paid in Euros (including 6 %% of taxes): %.2f €", amountToPay);
+		
+		
+		
+		
 		
 
 		sc.close();
