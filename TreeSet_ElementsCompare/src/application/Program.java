ee26977befd6e259047698ed215fde2cdc92d175
amac81 <arnaldocanelas@gmail.com>
2023-07-10 21:07:40 +0100

    compareTo method implementation in class Product

--- TreeSet_ElementsCompare/src/application/Program.java
+++ TreeSet_ElementsCompare/src/application/Program.java
@@ -1,29 +1,34 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
-		Set<Product> set = new HashSet<>();
+		Set<Product> set = new TreeSet<>();
 
 		set.add(new Product("Tv", 300.0));
 		set.add(new Product("Ipad", 1300.0));
 		set.add(new Product("Smartphone", 850.0));
 		set.add(new Product("BluRay player", 610.0));
 
-		Product p = new Product("Smartphone", 850.0);
-
-		/* if the object does not have the hashCode and equals methods implemented, Set
-		/* uses the pointer reference to compare the two objects.
+		/*
+		 * When using TreeSet, it is necessary that the elements (Objects) implement the
+		 * Comparable interface
 		 * 
-		 * Before we have implemented the hashCode and equals methods in the Product class 
-		 * the result of "set.contains(p)" would be false
+		 * In this example the Product class has to implement the Comparable interface
+		 * 
+		 * when printing the Set of Products, it will be sorted according to the
+		 * implementation of the compareTo method of the Product class. In this example
+		 * the products will be shown in alphabetical order by name
+		 * 
 		 */
-	
-		System.out.println(set.contains(p));
+
+		for (Product product : set) {
+			System.out.println(product);
+		}
 
 	}
 }
