e99be54338889979064819beaca49b483f3300a0
amac81 <arnaldocanelas@gmail.com>
2023-07-15 10:13:07 +0100

    better/simpler implementation

--- Map_Exercice01/src/application/Program.java
+++ Map_Exercice01/src/application/Program.java
@@ -3,66 +3,45 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
-
-import entities.Candidat;
-
 
 public class Program {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 
-		String filePath = "c:\\temp\\in.txt";
-		
+		String filePath = "/tmp/in.txt";
+
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-			
-			Map<String, Integer> votesMap = new HashMap<>(); // choose HashSet because it is fast and in this problem we don't need order
-			List<Candidat> candidats = new ArrayList<> ();
+
+			Map<String, Integer> votesMap = new LinkedHashMap<>();
 
 			String fileLine = br.readLine();
-			String [] fields;
+			String[] fields;
 			Integer nVotes = 0;
-		
-			//read from file to List;
+			String candidatName;
+
+			// read from file to List;
 			while (fileLine != null) {
 				fields = fileLine.split(",");
-				candidats.add(new Candidat(fields[0], Integer.parseInt(fields[1])));
+				candidatName = fields[0];
+				nVotes = Integer.parseInt(fields[1]);
+
+				if (votesMap.containsKey(candidatName)) {
+					votesMap.replace(candidatName, votesMap.get(candidatName) + nVotes);
+				} else {
+					votesMap.put(candidatName, nVotes);
+				}
+
 				fileLine = br.readLine();
 			}
 
-			for(Candidat c: candidats) {
-				votesMap.put(c.getName(), 0);
+			for (String key : votesMap.keySet()) {
+				System.out.println(key + ": " + votesMap.get(key));
 			}
-			
-			//sum number of votes by candidat
-			for(Candidat c: candidats) {
-				if(votesMap.containsKey(c.getName())) {
-					nVotes = votesMap.get(c.getName());
-					votesMap.put(c.getName(), c.getNumVotes() + nVotes);
-				}
-			}
-			
-			
-			Map<String, Integer> sortedMap = votesMap.entrySet().stream()
-                    .sorted(Entry.comparingByValue())
-                    .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e2, e1) -> e1, LinkedHashMap::new));
-			
-			for(String s: sortedMap.keySet()) {
-				System.out.printf("%s: %d%n", s ,sortedMap.get(s));
-			}
-			
-			
+
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 
-		
-		
 	}
 }
