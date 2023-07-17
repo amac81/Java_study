683ea28c1e6044757fc8b40042f992be2732f8cf
amac81 <arnaldocanelas@gmail.com>
2023-07-17 21:36:40 +0100

    department query

--- MySql_jdbc_ex01/src/application/Program.java
+++ MySql_jdbc_ex01/src/application/Program.java
@@ -1,19 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Locale;
 
 import db.DB;
 
 public class Program {
 	public static void main(String[] args) throws SQLException {
-		Locale.setDefault(Locale.US); 
-		
-		Connection dbConnection = DB.getDbConnection();
-		
-		System.out.println(dbConnection.getCatalog().toString());
-		
-		DB.closeConnection();
+		Locale.setDefault(Locale.US);
+
+		Connection dbConnection = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			dbConnection = DB.getDbConnection();
+			st = dbConnection.createStatement();
+			rs = st.executeQuery("SELECT * FROM department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			rs.close();
+			st.close();
+			DB.closeConnection();
+		}
+
 	}
 }
