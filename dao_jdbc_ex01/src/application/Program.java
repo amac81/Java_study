90567c1d948a9b16fa7d0d8386c783c33a545bb2
amac81 <arnaldocanelas@gmail.com>
2023-07-19 15:09:41 +0100

    dao_jdbc_ex01 1st commit

---
+++ dao_jdbc_ex01/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+
+	public static SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
+	public static void main(String[] args) throws SQLException {
+
+	}
+
+}
