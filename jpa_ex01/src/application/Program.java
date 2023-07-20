dfb1c13c72970f051a96a846ccff0bb3237a442f
amac81 <arnaldocanelas@gmail.com>
2023-07-20 19:06:07 +0100

    database.sql added

---
+++ jpa_ex01/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import domain.Person;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Person p1 = new Person(1, "Jonh Bill", "jonh@gmail.com");
+		Person p2 = new Person(2, "Mary Brown", "mary@gmail.com");
+		Person p3 = new Person(3, "Billy Bob", "billy@gmail.com");
+
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+	}
+
+}
