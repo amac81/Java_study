510f8834a3f32e230db902bd931c3635bc5c9192
amac81 <arnaldocanelas@gmail.com>
2023-07-21 12:03:42 +0100

    entity manager find

--- jpaexerc01/src/main/java/application/Program.java
+++ jpaexerc01/src/main/java/application/Program.java
@@ -9,23 +9,18 @@ import javax.persistence.EntityManager;
 public class Program {
 
 	public static void main(String[] args) {
-		Person p1 = new Person(null, "Jonh Bill", "jonh@gmail.com");
-		Person p2 = new Person(null, "Mary Brown", "mary@gmail.com");
-		Person p3 = new Person(null, "Billy Bob", "billy@gmail.com");
-
 		//persistence xml /src/main/resources/META-INF/persistence.xml
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
 		
-		System.out.println("done!");
+		Person p1 = em.find(Person.class, 1);
 		
+		System.out.println(p1);
+		
+		em.close();
+		emf.close();
  
 	}
 
