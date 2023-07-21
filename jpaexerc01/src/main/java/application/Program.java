c4c085019d3c398acf5cf0f063b00d66bfc1a986
amac81 <arnaldocanelas@gmail.com>
2023-07-21 11:59:57 +0100

    EntityManagerFactory and EntityManager

--- jpaexerc01/src/main/java/application/Program.java
+++ jpaexerc01/src/main/java/application/Program.java
@@ -1,17 +1,32 @@
 package application;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Person;
+import javax.persistence.EntityManager;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Person p1 = new Person(1, "Jonh Bill", "jonh@gmail.com");
-		Person p2 = new Person(2, "Mary Brown", "mary@gmail.com");
-		Person p3 = new Person(3, "Billy Bob", "billy@gmail.com");
+		Person p1 = new Person(null, "Jonh Bill", "jonh@gmail.com");
+		Person p2 = new Person(null, "Mary Brown", "mary@gmail.com");
+		Person p3 = new Person(null, "Billy Bob", "billy@gmail.com");
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3); 
+		//persistence xml /src/main/resources/META-INF/persistence.xml
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		
+		System.out.println("done!");
+		
+ 
 	}
 
 }
--- jpaexerc01/src/main/java/domain/Person.java
+++ jpaexerc01/src/main/java/domain/Person.java
@@ -2,9 +2,18 @@ package domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Person implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
